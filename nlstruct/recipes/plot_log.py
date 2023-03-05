3eee3c9769b539f759b0e03ce65a9d36a983321a
marconaguib <marco.naguib@universite-paris-saclay.fr>
2023-03-05 18:39:35 +0100

    feat : better plot

--- nlstruct/recipes/plot_log.py
+++ nlstruct/recipes/plot_log.py
@@ -22,6 +22,8 @@ dataset['word_count'] = dataset.groupby(['corpus','type_f1','xp_name','batch'])[
 #cumsum the word_count over every batch and seed
 dataset['word_count'] = dataset.groupby(['corpus','type_f1','xp_name','seed'])['word_count'].transform('cumsum')
 
+dataset = dataset.query('type_f1!="min"')
+
 def plot_iterations(data,**kwargs):
     sns.lineplot(data=data.query('batch<=20'), **kwargs)
 
@@ -32,20 +34,24 @@ def plot_all(data, **kwargs):
 hue_order = list(dataset['xp_name'].unique())
 hue_order= sorted(hue_order,key=lambda x: 0 if x.startswith('random') else 1)
 
-#for each corpus, stop the random plot at where the other plots stop
-if args.word_count and not args.extended:
-    max_word_count = dataset.groupby(['corpus']).apply(lambda x: x.query('xp_name!="random"')['word_count'].max())
-    max_word_count = max_word_count*1.1
-    for c in max_word_count.index:
-        dataset = dataset.query('corpus!="{}" or word_count<={} or batch>20'.format(c,max_word_count[c]))
+# #for each corpus, stop the random plot at where the other plots stop
+# if args.word_count and not args.extended:
+#     max_word_count = dataset.groupby(['corpus']).apply(lambda x: x.query('xp_name!="random"')['word_count'].max())
+#     max_word_count = max_word_count*1.1
+#     for c in max_word_count.index:
+#         dataset = dataset.query('corpus!="{}" or word_count<={} or batch>20'.format(c,max_word_count[c]))
 
 g = sns.FacetGrid(data=dataset, col='corpus', row='type_f1',sharey=True, sharex=False)
 g.map_dataframe(plot_iterations, x= 'batch' if not args.word_count else 'word_count', y="score", hue='xp_name', hue_order=hue_order)
 g.map_dataframe(plot_all,value='score',ls='--',c='black')
-g.add_legend(title='Selection strategy',)
+# put legend at the bottom
+g.add_legend(title='Stratégie de sélection',bbox_to_anchor=(0,0,1,0.5),loc='lower center',ncol=3)
+#tight layout
+g.fig.tight_layout()
 
 #set the titles of each facet
-g.set_titles(row_template='{row_name}', col_template='{col_name}')
+g.set_titles(row_template='f1_{row_name}', col_template='{col_name}')
+
 
 ax = plt.gca()
 ax.yaxis.set_major_locator(ticker.MultipleLocator(0.05))
@@ -53,6 +59,15 @@ for ax in g.axes.flat:
     ax.set_ylim(0,1)
     ax.set_xlabel('Iterations' if not args.word_count else 'Word count')
     ax.set_ylabel('F1 score')
-    
+
+#fix borders
+fig = plt.gcf()
+top=0.962
+bottom=0.145
+left=0.064
+right=0.982
+hspace=0.252
+wspace=0.033
+fig.subplots_adjust(top=top,bottom=bottom,left=left,right=right,hspace=hspace,wspace=wspace)
 plt.show()
 
