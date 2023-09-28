591ebb24aa3c4e866055d0186371c727ef4ee71b
marconaguib <marco.naguib@hotmail.com>
2023-09-28 14:33:08 +0200

    load_from_disk to deal with jean zay

--- nlstruct/datasets/huggingface.py
+++ nlstruct/datasets/huggingface.py
@@ -1,5 +1,5 @@
 from nlstruct.datasets.base import NERDataset
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 
 def tags_to_entities(words, ner_tags, tag_map):
     #this function takes a list of words and a list of tags and returns a list of entities
@@ -45,13 +45,17 @@ def load_from_hf(dataset, tag_map, doc_id_colname, words_colname='words', ner_ta
     return examples
 
 class HuggingfaceNERDataset(NERDataset):
-    def __init__(self, dataset_name: str, subset: str, tag_map: dict, preprocess_fn=None, doc_id_colname='doc_id', words_colname='words', ner_tags_colname='ner_tags'):
+    def __init__(self, dataset_name: str, subset: str, tag_map: dict, preprocess_fn=None, doc_id_colname='doc_id', words_colname='words', ner_tags_colname='ner_tags', load_from_disk=False):
+        self.load_from_disk = load_from_disk
         train_data, val_data, test_data = self.extract(dataset_name, subset, tag_map, doc_id_colname=doc_id_colname, words_colname=words_colname, ner_tags_colname=ner_tags_colname)
         super().__init__(train_data, val_data, test_data, preprocess_fn=preprocess_fn)
     
     def extract(self, dataset_name, subset, tag_map, doc_id_colname, words_colname, ner_tags_colname):
         try :
-            self.dataset = load_dataset(dataset_name, subset)
+            if self.load_from_disk:
+                self.dataset = load_dataset(dataset_name, subset)
+            else:
+                self.dataset = load_from_disk(f'$WORK/data/{dataset_name}/{subset}') 
         except ValueError:
             raise ValueError(f"Dataset {dataset_name} does not exist. Please check the name of the dataset.")
         train_data = load_from_hf(self.dataset["train"], tag_map, doc_id_colname=doc_id_colname, words_colname=words_colname, ner_tags_colname=ner_tags_colname)
